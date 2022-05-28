5589e71e09029de393a26dd392da02fc32a1388f
Cristiano Antunes <you@example.comcristiano.antunes@stud.hslu.ch>
2022-05-28 15:37:45 +0200

    Update 28.05

--- Python_Project.py
+++ Python_Project.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_excel(r'C:\Users\Crist\OneDrive - Hochschule Luzern\International Business Administration IBA\Semester 06\Programming with Phyton\Project_Python\Data\EU COUNTRIES_rail passengers_quarterly data since 2019-Q4.xlsx')
+data = pd.read_excel('Data\EU COUNTRIES_rail passengers_quarterly data since 2019-Q4.xlsx')
 df = pd.DataFrame(data, index=[2], columns=['Country','2021-Q4'])
 print("Here are the Values for: ")
 print(df)
