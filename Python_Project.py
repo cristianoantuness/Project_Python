31fe22b5410a4a663271f4ce1168d6ddb88a893e
Cristiano Antunes <you@example.comcristiano.antunes@stud.hslu.ch>
2022-05-30 20:51:22 +0200

    Final Version

--- Python_Project.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_excel('Data\EU COUNTRIES_rail passengers_quarterly data since 2019-Q4.xlsx')
-df = pd.DataFrame(data, index=[2], columns=['Country','2021-Q4'])
-print("Here are the Values for: ")
-print(df)
